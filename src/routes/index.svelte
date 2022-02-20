fa9639cbbd84ed197e1bc942d180d4923c71fcb0
ymo <ymo@ymolists>
2022-02-20 09:39:29 -0500

    initial import

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function load({ page }) {
+		await import('../lib/' + 'test' + '.svelte'); // does not work
+
+		return {
+			props: {}
+		};
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
